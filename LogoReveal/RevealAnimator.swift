0f3ed6370e3ed3e8748afac3aa4568ac6e4ca3ce
Lucas <lucaspedrazoli@gmail.com>
2021-03-01 14:48:40 -0300

    add pop animation

--- LogoReveal/RevealAnimator.swift
+++ LogoReveal/RevealAnimator.swift
@@ -21,27 +21,42 @@ UIViewControllerAnimatedTransitioning, CAAnimationDelegate {
 
   func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
     storedContext = transitionContext
-    let fromVC = transitionContext
-      .viewController(forKey: .from) as! MasterViewController
-    let toVC = transitionContext
-      .viewController(forKey: .to) as! DetailViewController
+    if operation == .push {
+      let fromVC = transitionContext
+        .viewController(forKey: .from) as! MasterViewController
+      let toVC = transitionContext
+        .viewController(forKey: .to) as! DetailViewController
 
-    transitionContext.containerView.addSubview(toVC.view)
-    toVC.view.frame = transitionContext.finalFrame(for: toVC)
+      transitionContext.containerView.addSubview(toVC.view)
+      toVC.view.frame = transitionContext.finalFrame(for: toVC)
 
-    let animation = CABasicAnimation(keyPath: "transform")
-    animation.fromValue = NSValue(caTransform3D: CATransform3DIdentity)
-    animation.toValue = NSValue(caTransform3D: CATransform3DConcat(CATransform3DMakeTranslation(0.0, -10.0, 0.0),      CATransform3DMakeScale(150.0, 150.0, 1.0)))
+      let animation = CABasicAnimation(keyPath: "transform")
+      animation.fromValue = NSValue(caTransform3D: CATransform3DIdentity)
+      animation.toValue = NSValue(caTransform3D: CATransform3DConcat(CATransform3DMakeTranslation(0.0, -10.0, 0.0),      CATransform3DMakeScale(150.0, 150.0, 1.0)))
 
-    animation.duration = animationDuration
-    animation.delegate = self
-    animation.fillMode = .forwards
-    animation.isRemovedOnCompletion = false
-    animation.timingFunction = CAMediaTimingFunction(name: .easeIn)
-    
-    let maskLayer: CAShapeLayer = RWLogoLayer.logoLayer()
-    maskLayer.position = fromVC.logo.position
-    toVC.view.layer.mask = maskLayer
-    maskLayer.add(animation, forKey: nil)
+      animation.duration = animationDuration
+      animation.delegate = self
+      animation.fillMode = .forwards
+      animation.isRemovedOnCompletion = false
+      animation.timingFunction = CAMediaTimingFunction(name: .easeIn)
+
+      let maskLayer: CAShapeLayer = RWLogoLayer.logoLayer()
+      maskLayer.position = fromVC.logo.position
+      toVC.view.layer.mask = maskLayer
+      maskLayer.add(animation, forKey: nil)
+
+      fromVC.logo.add(animation, forKey: nil)
+    }
+  }
+
+  func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+    if let context = storedContext {
+      context.completeTransition(!context.transitionWasCancelled)
+      let fromVC = context.viewController(forKey: .from) as! MasterViewController
+      fromVC.logo.removeAllAnimations()
+      let toVC = context.viewController(forKey: .to) as! DetailViewController
+      toVC.view.layer.mask = nil
+    }
+    storedContext = nil
   }
 }
