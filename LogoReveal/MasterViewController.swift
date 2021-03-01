3e978a3adccc063270ca3ad1a63508da42e99a4f
Lucas <lucaspedrazoli@gmail.com>
2021-03-01 14:40:10 -0300

    create navigation animation

--- LogoReveal/MasterViewController.swift
+++ LogoReveal/MasterViewController.swift
@@ -1,25 +1,3 @@
-/*
-* Copyright (c) 2014-present Razeware LLC
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*/
-
 import UIKit
 import QuartzCore
 
@@ -30,11 +8,12 @@ func delay(seconds: Double, completion: @escaping ()-> Void) {
 class MasterViewController: UIViewController {
   
   let logo = RWLogoLayer.logoLayer()
+  let transition = RevealAnimator()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     title = "Start"
-
+    navigationController?.delegate = self
   }
   
   override func viewDidAppear(_ animated: Bool) {
@@ -57,6 +36,15 @@ class MasterViewController: UIViewController {
   @objc func didTap() {
     performSegue(withIdentifier: "details", sender: nil)
   }
-  
+}
+
+extension MasterViewController: UINavigationControllerDelegate {
+  func navigationController(_ navigationController: UINavigationController,
+                            animationControllerFor operation: UINavigationController.Operation,
+                            from fromVC: UIViewController,
+                            to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    transition.operation = operation
+    return transition
+  }
 }
 
---
+++ LogoReveal/RevealAnimator.swift
@@ -0,0 +1,47 @@
+//
+//  RevealAnimator.swift
+//  LogoReveal
+//
+//  Created by Lucas Pedrazoli on 01/03/21.
+//  Copyright © 2021 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class RevealAnimator: NSObject,
+UIViewControllerAnimatedTransitioning, CAAnimationDelegate {
+
+  let animationDuration = 2.0
+  var operation: UINavigationController.Operation = .push
+  weak var storedContext: UIViewControllerContextTransitioning?
+
+  func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+    return animationDuration
+  }
+
+  func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+    storedContext = transitionContext
+    let fromVC = transitionContext
+      .viewController(forKey: .from) as! MasterViewController
+    let toVC = transitionContext
+      .viewController(forKey: .to) as! DetailViewController
+
+    transitionContext.containerView.addSubview(toVC.view)
+    toVC.view.frame = transitionContext.finalFrame(for: toVC)
+
+    let animation = CABasicAnimation(keyPath: "transform")
+    animation.fromValue = NSValue(caTransform3D: CATransform3DIdentity)
+    animation.toValue = NSValue(caTransform3D: CATransform3DConcat(CATransform3DMakeTranslation(0.0, -10.0, 0.0),      CATransform3DMakeScale(150.0, 150.0, 1.0)))
+
+    animation.duration = animationDuration
+    animation.delegate = self
+    animation.fillMode = .forwards
+    animation.isRemovedOnCompletion = false
+    animation.timingFunction = CAMediaTimingFunction(name: .easeIn)
+    
+    let maskLayer: CAShapeLayer = RWLogoLayer.logoLayer()
+    maskLayer.position = fromVC.logo.position
+    toVC.view.layer.mask = maskLayer
+    maskLayer.add(animation, forKey: nil)
+  }
+}
